632fbdadd13a694b27dba7936b1dea92a72ac357
Muhammad Shameel <123203015+MuhammadShameel@users.noreply.github.com>
2025-10-22 11:33:16 +0500

    Remove unused theme variables in globals.css and adjust container styles for better layout; add LinkedInRevenue component to Home page

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import HeroSection from "./components/HeroSection";
 import GenerateLeads from "./components/GenerateLeads";
+import LinkedInRevenue from "./components/LinkedInRevenue";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <GenerateLeads />
+      <LinkedInRevenue />
     </>
   );
 }
