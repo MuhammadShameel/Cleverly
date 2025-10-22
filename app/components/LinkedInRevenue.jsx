b9bec784b4e45c014eb29157ef9966a354b70d5c
Muhammad Shameel <123203015+MuhammadShameel@users.noreply.github.com>
2025-10-22 11:33:46 +0500

    Add LinkedInRevenue component to showcase lead generation services with responsive layout and images

---
+++ app/components/LinkedInRevenue.jsx
@@ -0,0 +1,342 @@
+import React from "react";
+import Image from "next/image";
+
+import Search from "../../public/images/card-search.png";
+import Write from "../../public/images/write.png";
+import Internet from "../../public/images/internet.png";
+import Deal from "../../public/images/deal.png";
+
+const LinkedInRevenue = () => {
+  return (
+    <>
+      <section className="lg:px-5 md:px-4 px-3 py-20 ">
+        <div className="container mx-auto">
+          <div className="text-center max-w-4xl mx-auto">
+            <h2>
+              We help turn your LinkedIn into a revenue driving machine in these
+              4 areas
+            </h2>
+            <p className="mt-5">
+              Turn your LinkedIn profile into a lead converting sales machine.
+              Our simple to use features combined with our done-for-you services
+              will enable you to generate and close deals faster. LinkedIn
+              Appointment Setting Our team of dedicated response handlers will
+              save you dozens of hours per month by handling your inbox,
+              nurturing leads, and getting meetings booked directly on your
+              calendar.
+            </p>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-16 py-6 mt-10">
+            <div className="flex flex-col items-center text-center py-7 px-7 rounded-sm shadow-[0_1.49px_5px_#7d6ba40d,_0_12.06px_17.87px_#6320ee05,_0_22.56px_33.42px_#6320ee05,_0_1.49px_2.21px_#7d6ba40d] bg-gradient-to-b from-[#fdfcff] to-[#faf8ff] border border-solid border-[#eff3f6]">
+              <svg
+                width="47"
+                height="46"
+                viewBox="0 0 47 46"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="0.5"
+                  width="46"
+                  height="46"
+                  rx="3"
+                  fill="url(#paint0_linear_93_2479)"
+                />
+                <path
+                  d="M14.1249 19.875H35.3749"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M17.8747 13.625H13.861C13.2332 13.6608 12.6453 13.9441 12.2261 14.4127C11.8069 14.8814 11.5906 15.4971 11.6247 16.125V31.125C11.5906 31.7528 11.8069 32.3686 12.2261 32.8373C12.6453 33.3059 13.2332 33.5892 13.861 33.625H33.1385C33.7662 33.5892 34.3542 33.3059 34.7734 32.8373C35.1926 32.3686 35.4088 31.7528 35.3747 31.125V16.125C35.4088 15.4971 35.1926 14.8814 34.7734 14.4127C34.3542 13.9441 33.7662 13.6608 33.1385 13.625H31.6247"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M29.1249 11.125V16.125"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M17.8749 11.125V16.125"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M20.3749 13.6254L29.1249 13.6079"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <defs>
+                  <linearGradient
+                    id="paint0_linear_93_2479"
+                    x1="23.5"
+                    y1="0"
+                    x2="23.5"
+                    y2="46"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stopColor="#F5F1FF" />
+                    <stop offset="1" stopColor="#EAE3FF" />
+                  </linearGradient>
+                </defs>
+              </svg>
+              <div className="content-wrapper mt-5">
+                <h3 className="mb-2 !font-bold text-black">
+                  LinkedIn Appointment Setting
+                </h3>
+                <p className="text-gray-600 !text-[16px]">
+                  Our team of dedicated response handlers will save you dozens
+                  of hours per month by handling your inbox, nurturing leads,
+                  and getting meetings booked directly on your calendar.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center text-center py-7 px-7 rounded-sm shadow-[0_1.49px_5px_#7d6ba40d,_0_12.06px_17.87px_#6320ee05,_0_22.56px_33.42px_#6320ee05,_0_1.49px_2.21px_#7d6ba40d] bg-gradient-to-b from-[#fdfcff] to-[#faf8ff] border border-solid border-[#eff3f6]">
+              <svg
+                width="47"
+                height="46"
+                viewBox="0 0 47 46"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="0.5"
+                  width="46"
+                  height="46"
+                  rx="3"
+                  fill="url(#paint0_linear_93_2479)"
+                />
+                <path
+                  d="M14.1249 19.875H35.3749"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M17.8747 13.625H13.861C13.2332 13.6608 12.6453 13.9441 12.2261 14.4127C11.8069 14.8814 11.5906 15.4971 11.6247 16.125V31.125C11.5906 31.7528 11.8069 32.3686 12.2261 32.8373C12.6453 33.3059 13.2332 33.5892 13.861 33.625H33.1385C33.7662 33.5892 34.3542 33.3059 34.7734 32.8373C35.1926 32.3686 35.4088 31.7528 35.3747 31.125V16.125C35.4088 15.4971 35.1926 14.8814 34.7734 14.4127C34.3542 13.9441 33.7662 13.6608 33.1385 13.625H31.6247"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M29.1249 11.125V16.125"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M17.8749 11.125V16.125"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M20.3749 13.6254L29.1249 13.6079"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <defs>
+                  <linearGradient
+                    id="paint0_linear_93_2479"
+                    x1="23.5"
+                    y1="0"
+                    x2="23.5"
+                    y2="46"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stopColor="#F5F1FF" />
+                    <stop offset="1" stopColor="#EAE3FF" />
+                  </linearGradient>
+                </defs>
+              </svg>
+              <div className="content-wrapper mt-5">
+                <h3 className="mb-2 !font-bold text-black">
+                  LinkedIn Appointment Setting
+                </h3>
+                <p className="text-gray-600 !text-[16px]">
+                  Our team of dedicated response handlers will save you dozens
+                  of hours per month by handling your inbox, nurturing leads,
+                  and getting meetings booked directly on your calendar.
+                </p>
+              </div>
+            </div>{" "}
+            <div className="flex flex-col items-center text-center py-7 px-7 rounded-sm shadow-[0_1.49px_5px_#7d6ba40d,_0_12.06px_17.87px_#6320ee05,_0_22.56px_33.42px_#6320ee05,_0_1.49px_2.21px_#7d6ba40d] bg-gradient-to-b from-[#fdfcff] to-[#faf8ff] border border-solid border-[#eff3f6]">
+              <svg
+                width="47"
+                height="46"
+                viewBox="0 0 47 46"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="0.5"
+                  width="46"
+                  height="46"
+                  rx="3"
+                  fill="url(#paint0_linear_93_2479)"
+                />
+                <path
+                  d="M14.1249 19.875H35.3749"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M17.8747 13.625H13.861C13.2332 13.6608 12.6453 13.9441 12.2261 14.4127C11.8069 14.8814 11.5906 15.4971 11.6247 16.125V31.125C11.5906 31.7528 11.8069 32.3686 12.2261 32.8373C12.6453 33.3059 13.2332 33.5892 13.861 33.625H33.1385C33.7662 33.5892 34.3542 33.3059 34.7734 32.8373C35.1926 32.3686 35.4088 31.7528 35.3747 31.125V16.125C35.4088 15.4971 35.1926 14.8814 34.7734 14.4127C34.3542 13.9441 33.7662 13.6608 33.1385 13.625H31.6247"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M29.1249 11.125V16.125"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M17.8749 11.125V16.125"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M20.3749 13.6254L29.1249 13.6079"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <defs>
+                  <linearGradient
+                    id="paint0_linear_93_2479"
+                    x1="23.5"
+                    y1="0"
+                    x2="23.5"
+                    y2="46"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stopColor="#F5F1FF" />
+                    <stop offset="1" stopColor="#EAE3FF" />
+                  </linearGradient>
+                </defs>
+              </svg>
+              <div className="content-wrapper mt-5">
+                <h3 className="mb-2 !font-bold text-black">
+                  LinkedIn Appointment Setting
+                </h3>
+                <p className="text-gray-600 !text-[16px]">
+                  Our team of dedicated response handlers will save you dozens
+                  of hours per month by handling your inbox, nurturing leads,
+                  and getting meetings booked directly on your calendar.
+                </p>
+              </div>
+            </div>{" "}
+            <div className="flex flex-col items-center text-center py-7 px-7 rounded-sm shadow-[0_1.49px_5px_#7d6ba40d,_0_12.06px_17.87px_#6320ee05,_0_22.56px_33.42px_#6320ee05,_0_1.49px_2.21px_#7d6ba40d] bg-gradient-to-b from-[#fdfcff] to-[#faf8ff] border border-solid border-[#eff3f6]">
+              <svg
+                width="47"
+                height="46"
+                viewBox="0 0 47 46"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="0.5"
+                  width="46"
+                  height="46"
+                  rx="3"
+                  fill="url(#paint0_linear_93_2479)"
+                />
+                <path
+                  d="M14.1249 19.875H35.3749"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M17.8747 13.625H13.861C13.2332 13.6608 12.6453 13.9441 12.2261 14.4127C11.8069 14.8814 11.5906 15.4971 11.6247 16.125V31.125C11.5906 31.7528 11.8069 32.3686 12.2261 32.8373C12.6453 33.3059 13.2332 33.5892 13.861 33.625H33.1385C33.7662 33.5892 34.3542 33.3059 34.7734 32.8373C35.1926 32.3686 35.4088 31.7528 35.3747 31.125V16.125C35.4088 15.4971 35.1926 14.8814 34.7734 14.4127C34.3542 13.9441 33.7662 13.6608 33.1385 13.625H31.6247"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M29.1249 11.125V16.125"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M17.8749 11.125V16.125"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M20.3749 13.6254L29.1249 13.6079"
+                  stroke="#4000FF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <defs>
+                  <linearGradient
+                    id="paint0_linear_93_2479"
+                    x1="23.5"
+                    y1="0"
+                    x2="23.5"
+                    y2="46"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stopColor="#F5F1FF" />
+                    <stop offset="1" stopColor="#EAE3FF" />
+                  </linearGradient>
+                </defs>
+              </svg>
+              <div className="content-wrapper mt-5">
+                <h3 className="mb-2 !font-bold text-black">
+                  LinkedIn Appointment Setting
+                </h3>
+                <p className="text-gray-600 !text-[16px]">
+                  Our team of dedicated response handlers will save you dozens
+                  of hours per month by handling your inbox, nurturing leads,
+                  and getting meetings booked directly on your calendar.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="btn-wrapper text-center">
+            <button className="btn btn-secondary mt-10">
+              Free Consultation
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default LinkedInRevenue;
