799fbe7c2989e70a4cf7c75717e62d639fc4de40
Muhammad Shameel <123203015+MuhammadShameel@users.noreply.github.com>
2025-10-23 16:00:24 +0500

    Add FooterBanner component to promote LinkedIn revenue generation with a call-to-action button

---
+++ app/components/FooterBanner.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const footerBanner = () => {
+  return (
+    <>
+      <section className="hero-section bg-gradient-to-b from-[#0d0033] to-[#3301cc] py-20 text-white lg:px-5 md:px-4 px-3">
+        <div className="container mx-auto">
+          <div className="text-center mx-auto">
+            <div className="content-wrapper">
+              <h1 className="font-bold max-w-4xl mx-auto">
+                Make your LinkedIn a revenue driving machine
+              </h1>
+              <button className="btn btn-orange mt-10">Free Consultaion</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default footerBanner;
