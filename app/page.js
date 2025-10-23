5424a829b22204990402f6b7db7cd442dc7a84a9
Muhammad Shameel <123203015+MuhammadShameel@users.noreply.github.com>
2025-10-23 16:00:42 +0500

    Add LinkedInSales, Packages, and FooterBanner components to Home page for improved lead generation

--- app/page.js
+++ app/page.js
@@ -4,6 +4,9 @@ import HeroSection from "./components/HeroSection";
 import GenerateLeads from "./components/GenerateLeads";
 import LinkedInRevenue from "./components/LinkedInRevenue";
 import LinkedInMarketing from "./components/LinkedInMarketing";
+import LinkedInSales from "./components/LinkedInSales";
+import Packages from "./components/Packages";
+import FooterBanner from "./components/FooterBanner.jsx";
 
 export default function Home() {
   return (
@@ -12,6 +15,9 @@ export default function Home() {
       <GenerateLeads />
       <LinkedInRevenue />
       <LinkedInMarketing />
+      <LinkedInSales />
+      <Packages />
+      <FooterBanner />
     </>
   );
 }
